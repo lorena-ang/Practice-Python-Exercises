7620d673c4a7a300aa7e826119e96cabfefa7050
lorena-ang <45380244+lorena-ang@users.noreply.github.com>
2018-12-04 16:52:34 -0600

    Update 01-Character-Input.py

--- 01-Character-Input.py
+++ 01-Character-Input.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 # Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old. 
 # Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.Print out that many copies of the previous message on separate lines.
 
