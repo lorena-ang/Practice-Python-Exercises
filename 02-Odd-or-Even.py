4d49b51f7a5692f6b62a0ac954c0cca6aece5b37
lorena-ang <45380244+lorena-ang@users.noreply.github.com>
2018-12-04 16:52:20 -0600

    Update 02-Odd-or-Even.py

--- 02-Odd-or-Even.py
+++ 02-Odd-or-Even.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 # Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. If the number is a multiple of 4, print out a different message.
 # Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 
