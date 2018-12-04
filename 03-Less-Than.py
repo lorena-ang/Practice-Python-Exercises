fa3ed87bf820a5b96f6c542f0777e4b3fe2578f6
lorena-ang <45380244+lorena-ang@users.noreply.github.com>
2018-12-04 16:52:52 -0600

    Update 03-Less-Than.py

--- 03-Less-Than.py
+++ 03-Less-Than.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 # Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]. Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 # Instead of printing the elements one by one, make a new list that has all the elements in it and print out this new list. Write this in one line of code.
 
